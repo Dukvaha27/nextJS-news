5c30838bdf83ef90d0b4a9390dfc8d20763046b1
Dukvakha <dukvakha1997@mail.ru>
2021-11-28 01:01:17 +0300

    доработка функции change language

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 import { NextPage } from "next";
 import Link from "next/link";
-import itmoLogo from "../public/itmo_logo.svg";
 import Image from "next/image";
+import itmoLogo from "../public/itmo_logo.svg";
 import { useRouter } from "next/router";
 
 interface HProps {
@@ -24,20 +24,19 @@ const Layout: NextPage<HProps> = ({ children }) => {
         className={"h-10 flex flex-wrap items-center justify-around"}
         style={{ background: "linear-gradient(90deg, #3951e7, #832ab9)" }}
       >
-        <div className={"cursor-pointer"}>
-          <Link href="/">
-            <Image src={itmoLogo} alt="" />
-          </Link>
+        <div className={"cursor-pointer"} onClick={() => router.push("/")}>
+          <Image src={itmoLogo} alt="" />
         </div>
         <select
+          defaultValue={lang}
           value={lang}
           onChange={handleChangeLang}
           style={{ backgroundColor: "inherit", color: "white" }}
         >
-          <option className={"text-black"} value="/ru">
+          <option disabled={lang === '/ru'} className={"text-black"} value="/ru">
             Русский
           </option>
-          <option className={"text-black"} value="/en">
+          <option disabled={lang === '/en'} className={"text-black"} value="/en">
             Английский
           </option>
         </select>
